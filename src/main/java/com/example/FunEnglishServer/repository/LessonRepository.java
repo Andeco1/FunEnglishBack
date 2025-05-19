627a0ce4655bdb2b990c16dae2e9b410f2b320de
Yan <Andeco12@yandex.ru>
2025-05-19 15:42:43 +0300

    hands-off

---
+++ src/main/java/com/example/FunEnglishServer/repository/LessonRepository.java
@@ -0,0 +1,11 @@
+package com.example.FunEnglishServer.repository;
+
+import com.example.FunEnglishServer.model.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LessonRepository extends JpaRepository<Lesson, Long> {
+}
